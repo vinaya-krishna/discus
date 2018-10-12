220c333e7fe9cb2419ec4c7be0031c5ee3716123
“vinaykp” <vinaya.krishna.dev@gmail.com>
2018-10-11 20:19:26 -0700

    signup basics

--- boards/views.py
+++ boards/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 # Create your views here.
 
 
@@ -17,22 +18,42 @@ def board_topics(request, id):
 def new_topic(request, id):
     board = get_object_or_404(Board, pk=id)
     if(request.method == "POST"):
-        subject = request.POST['subject']
-        message = request.POST['message']
+        # subject = request.POST['subject']
+        # message = request.POST['message']
+
         user = User.objects.first()  # TODO: get the currently logged in user
 
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
+        # topic = Topic.objects.create(
+        #     subject=subject,
+        #     board=board,
+        #     starter=user
+        # )
 
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
+        # post = Post.objects.create(
+        #     message=message,
+        #     topic=topic,
+        #     created_by=user
+        # )
 
-        return redirect('board_topics', id=board.pk)  # TODO: redirect to the created topic page
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', id=board.pk)
+    else:
+        form = NewTopicForm()
 
-    return render(request, 'new_topic.html', {'board': board})
+       
+
+          # TODO: redirect to the created topic page
+
+    # return render(request, 'new_topic.html', {'board': board})
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
